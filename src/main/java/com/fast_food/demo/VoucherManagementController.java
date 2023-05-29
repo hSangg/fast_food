9c22399f9394757e975ed361e05cb6ad40fd33e8
sang <sangfc774@gmail.com>
2023-05-29 23:34:01 +0700

    adding

--- src/main/java/com/fast_food/demo/VoucherManagementController.java
+++ src/main/java/com/fast_food/demo/VoucherManagementController.java
@@ -1,2 +1,13 @@
-package com.fast_food.demo;public class VoucherManagementController {
+package com.fast_food.demo;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VoucherManagementController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
